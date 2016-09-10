909753c39462444cc78fff6da738706e47acf385
Premysl Donat <pdonat@seznam.cz>
2016-09-10 13:07:52 +0200

    wip

--- Models.elm
+++ Models.elm
@@ -1,24 +1,23 @@
 -- module Models exposing (Model, initialModel)
 module Models exposing (Model, initialModel2)
 
--- type alias Attributes = Children (List Model)
-
 type alias Attribute = { name: String, value: String }
 
-type Model = Model { tag: String, attributes: List Attribute, children: List Model }
+type alias Model = { tag: String, attributes: List Attribute, children: Children, value: String }
+type Children = Children (List Model)
 
-initialModel =
-  Model { tag = "input", attributes = [], children = [] }
+-- initialModel2 =
+--   { tag = "input", attributes = [], children = [] }
 
 initialModel2 =
-  Model { tag = "form", attributes = [], children = initialChildren }
+  { tag = "form", attributes = [], children = initialChildren }
 
 initialChildren =
   let
-    label = Model { tag = "label", attributes = [{ name = "for", value = "input1" }], children = [] }
-    input = Model { tag = "input", attributes = [{ name = "type", value = "text" }, { name = "class", value = "form-control" }, { name = "id", value = "input1" }], children = [] }
-    wholeInput = Model { tag = "div", attributes = [{ name = "class", value = "form-group" }], children = [label, input] }
+    label = { tag = "label", attributes = [Attribute "for" "input1"], children = [], value = "" }
+    input = { tag = "input", attributes = [Attribute "type" "text", Attribute "class" "form-control", Attribute "id" "input1"], children = [], value = "" }
+    wholeInput = { tag = "div", attributes = [Attribute "class" "form-group"], children = [label, input], value = "" }
 
-    submit = Model { tag = "button", attributes = [{ name = "type", value = "submit" }, { name = "class", value = "btn btn-primary"}], children = [Model { tag = "_rawText", attributes = [{ name = "value", value = "Submit" }], children = [] } ]}
+    submit = { tag = "button", attributes = [Attribute "type" "submit", Attribute "class" "btn btn-primary"], children = [], value = "Submit"}
   in
     [wholeInput, submit]
